acd682eae24b46c59bb8e4af5450af09cf2f2205
prompt-study <prompttasks2@gmail.com>
2025-06-02 12:33:40 -0300

    Add files via upload

---
+++ tools/evaluation/CodeBleu/parser/build.py
@@ -0,0 +1,18 @@
+# Copyright (c) Microsoft Corporation. 
+# Licensed under the MIT license.
+
+from tree_sitter import Language, Parser
+
+Language.build_library(
+  'my-languages.so',
+  [
+    # 'tree-sitter-go',
+    # 'tree-sitter-javascript',
+    'tree-sitter-python',
+    # 'tree-sitter-php',
+    'tree-sitter-java',
+    # 'tree-sitter-ruby',
+    'tree-sitter-c-sharp',
+  ]
+)
+
